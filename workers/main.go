7dcd514b2f86eb7ac5ce15d8b7cb8c46420d0186
strelnykov <strelnykov@qarea.com>
2019-07-24 15:36:06 +0300

    update workers

--- workers/main.go
+++ workers/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
 	"runtime"
+	"time"
 )
 
-const url = "https://golang.org/"
+const url = "http://awsdeploy-app.fpw7ywc3xp.eu-central-1.elasticbeanstalk.com/hello"
 
 func main() {
-	requests := 100
+	requests := 10000
+	fmt.Println("requests to do: ", requests)
+	time.Sleep(3 * time.Second)
 
 	jobs := make(chan struct{}, requests)
 	results := make(chan struct{}, requests)
@@ -38,7 +43,15 @@ func worker(id int, jobs <-chan struct{}, results chan<- struct{}) {
 	var counter int
 	fmt.Printf("init worker № %d\n", id)
 	for range jobs {
-		_, _ = http.Get(url)
+		t := rand.Intn(100)
+		time.Sleep(time.Duration(t) * time.Millisecond)
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Println(err)
+		}
+		if resp != nil && resp.StatusCode != 200 {
+			log.Println(resp.Status)
+		}
 		fmt.Printf("worker № %d finished job \n", id)
 		counter++
 		results <- struct{}{}
